97d48369c6da033bf3d54bc603ae7f359afcf681
FLI\cflint <cflint@flhi.com>
2019-06-30 13:48:39 -0400

    flask stuff

--- app.py
+++ app.py
@@ -2,9 +2,30 @@
 from flask import Flask, flash, jsonify, render_template, request
 import webbrowser
 from flask_cors import CORS, cross_origin
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras.models import load_model
+from keras.utils import CustomObjectScope
+from keras.initializers import glorot_uniform
+from keras.preprocessing.sequence import pad_sequences
+from cypher import encode
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+CORS(app)
+
+# load the model, and pass in the custom metric function
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+       model = load_model('first_model_50.h5')
+
+model.summary()
+
 @app.route('/')
 def hello():
     # return "this webpage is working"
@@ -19,9 +40,19 @@ def my_form_post():
 
 @app.route("/getvalue/<title>/<body>")
 def data(title, body):
+    body = body.replace("%20", " ")
+    title = title.replace("%20", " ")
+    tq = [{'Title':title,'Body':body}]
+    df = pd.DataFrame(tq)
+
+    df['indexed'] = df.apply(lambda x: encode(x['Title'], x['Body']), axis=1)
+    final = pad_sequences(df['indexed'], value=0, padding='post', maxlen=50)
+    # model._make_predict_function()
+    f = model.predict_classes(final)
+
     """Display all data from the collection."""
-    # return f'T :{title} and B : {body}'
-    return f'T --- {title} and B --- {body}'
+    return f'B : {f}'
+    # return f'prediction --- {f}'
 
 # @app.route('/handle_data' ,methods = ['POST', 'GET'])    
 # def handle_data():
